06cd19664362fd2a32664c068d75f51a67ba6f12
KroL <krol@kerat.ru>
2016-09-17 10:48:35 +0300

    Enum descriptions

--- Market.Bot/BotApiClient/Contracts/DateRange.cs
+++ Market.Bot/BotApiClient/Contracts/DateRange.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,19 @@ namespace Market.Bot.BotApiClient.Contracts
 {
     public enum DateRange
     {
+        [Description("за вчера")]
         yesterday = 0,
+
+        [Description("за сегодня")]
         today = 1,
+
+        [Description("за неделю")]
         week = 2,
+
+        [Description("за месяц")]
         month = 3,
+
+        [Description("за год")]
         year = 4
     }
 }
--- Market.Bot/BotApiClient/Contracts/Sections.cs
+++ Market.Bot/BotApiClient/Contracts/Sections.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,14 @@ namespace Market.Bot.BotApiClient.Contracts
 {
     public enum Sections
     {
+        [Description("по Москве")]
         Default = 0,
+        [Description("по Московской области")]
         EasuzMoscowRegion = 1,
         Ekb = 2,
+        [Description("по Волгограду")]
         Volgograd = 3,
+        [Description("по Волгоградской области")]
         VolgogradRegion = 4,
         IrkutskRegion = 5,
         TyumenRegion = 6,
